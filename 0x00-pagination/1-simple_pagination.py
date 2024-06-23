f34d92fc4ffdd184def14040dea1f85399dfaf1d
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-23 23:57:02 +0300

    test

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python3
+"""
+Defines class Server that paginates a database of popular baby names
+"""
+
+
+import requests
 import csv
 import math
 from typing import List
 
 
 def index_range(page, page_size):
-    """ index range function """
+    """Return a tuple of size two
+    containing a start index and an end index
 
+    Args:
+        page (INT): corresponding to the range of indexes
+        page_size (INT): corresponding to the range of indexes
+    """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
@@ -31,14 +43,25 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Return a list of page rows """
+        """Return the appropriate page of the dataset
+        (i.e. the correct list of rows).
+
+        Args:
+            page (int, optional): Defaults to 1.
+            page_size (int, optional): Defaults to 10.
+
+        Returns:
+            List[List]: _description_
+        """
+        dataset = self.dataset()
+
         assert isinstance(page, int)
         assert isinstance(page_size, int)
+
         assert page > 0
         assert page_size > 0
-        dataset = self.dataset()
         try:
-            limits = index_range(page, page_size)
-            return dataset[limits[0]:limits[1]]
-        except Exception:
+            indexing = index_range(page, page_size)
+            return dataset[indexing[0]:indexing[1]]
+        except Exception as e:
             return []
