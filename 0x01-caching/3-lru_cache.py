3400105b401430a431e14926ec664e11b4d9d0a9
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-26 01:17:09 +0300

    task3_bugfix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,20 +14,20 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """ Add key, Item to cache.data """
-        if key is None or item is None:
-            return
-        if key in self.cache_data:
+        if key is not None or item is not None:
             self.cache_data[key] = item
-            self.access_list.append(
-                self.access_list.pop(self.access_list.index(key)))
-            return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            LR_key = self.access_list[0]
-            print(f"DISCARD: {LR_key}")
-            del self.cache_data[LR_key]
-            self.access_list.pop(0)
-        self.access_list.append(key)
-        self.cache_data[key] = item
+            if key not in self.access_list:
+                self.access_list.append(key)
+            else:
+                self.access_list.append(
+                    self.access_list.pop(
+                        self.access_list.index(key)
+                    )
+                )
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed_item = self.access_list.pop(0)
+                del self.cache_data[removed_item]
+                print("DISCARD: {}".format(removed_item))
 
     def get(self, key):
         """ get item of key from cache.data """
