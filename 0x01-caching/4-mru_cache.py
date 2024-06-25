8c85ba2e4f5ba6b7e2df715e3c87498ec47e3fc0
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-26 01:02:51 +0300

    task4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" MRUCache module
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache Class"""
+
+    def __init__(self):
+        """ Inistantiation method """
+        super().__init__()
+        self.access_list = []
+
+    def put(self, key, item):
+        """ Add key, Item to cache.data """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.access_list.append(
+                self.access_list.pop(self.access_list.index(key)))
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            MR_key = self.access_list[-1]
+            print(f"DISCARD: {MR_key}")
+            del self.cache_data[MR_key]
+            self.access_list.pop()
+        self.access_list.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get item of key from cache.data """
+        if key not in self.cache_data:
+            return None
+        return self.cache_data[key]
