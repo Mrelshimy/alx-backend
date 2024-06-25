d6b46dc6ec8e182ce23f02e427a4b680b284b452
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-26 01:30:31 +0300

    bugfix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,7 +10,7 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Inistantiation method """
         super().__init__()
-        self.access_dict = {}
+        self.access_list = []
 
     def put(self, key, item):
         """ Add key, Item to cache.data """
@@ -18,19 +18,21 @@ class LFUCache(BaseCaching):
             return
         if key in self.cache_data:
             self.cache_data[key] = item
-            self.access_dict += 1
+            self.access_list.append(
+                self.access_list.pop(self.access_list.index(key)))
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            LF_key = list(sorted(self.access_dict))[-1]
+            LF_key = self.access_list[0]
             print(f"DISCARD: {LF_key}")
             del self.cache_data[LF_key]
-            self.access_dict[LF_key]
-        self.access_dict[key] = 1
+            self.access_list.pop(0)
+        self.access_list.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
         """ get item of key from cache.data """
         if key is not None and key in self.cache_data.keys():
-            self.access_dict[key] += 1
+            self.access_list.append(
+                self.access_list.pop(self.access_list.index(key)))
             return self.cache_data.get(key)
         return None
