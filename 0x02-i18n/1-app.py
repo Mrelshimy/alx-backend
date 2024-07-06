9b7a2f7ee01eda3b6a01fa48951c125a0ad7371a
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-06 15:20:52 +0300

    pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,13 +7,16 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config():
     """ Basic config class """
-    LANGUAGES =['en', 'fr']
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE ='UTC'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
+
 
 @app.route("/")
 def index():
