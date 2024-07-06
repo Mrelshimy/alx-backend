8a0fd082561a943d525eb351311568a6505e6da1
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-06 15:18:49 +0300

    task1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Flask app module """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config():
+    """ Basic config class """
+    LANGUAGES =['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE ='UTC'
+
+app.config.from_object(Config)
+
+@app.route("/")
+def index():
+    """ main view """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
